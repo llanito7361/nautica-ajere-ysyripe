ee775b84b1791ea2b34d64529ab8ab2dee51e7a8
Emanuel Llano <llano.code.log@gmail.com>
2026-08-18 18:16:53 -0300

    asd

--- app/components/AsideButtons/AsideButtons.jsx
+++ app/components/AsideButtons/AsideButtons.jsx
@@ -1,9 +1,13 @@
 import "./AsideButtons.css";
-import { facebook, instagram, whatsapp } from "../../../public/assets/icons/index";
+import {
+  facebook,
+  instagram,
+  whatsapp,
+} from "../../../public/assets/icons/index";
 import Image from "next/image";
 const URL_FACEBOOK =
   "https://web.facebook.com/Dr.Notebooks/?locale=es_LA&_rdc=1&_rdr";
-const URL_INSTAGRAM = "instagram.com/direct/inbox/";
+const URL_INSTAGRAM = "https://www.instagram.com/nautica.ajere.ysyrype/";
 
 const AsideButtons = () => {
   return (
@@ -14,16 +18,18 @@ const AsideButtons = () => {
         </a>
       </div>
       <div className="button instagram">
-      <a rel="noreferrer noopener" target="_blank" href={URL_INSTAGRAM}>
-        <Image src={instagram} alt="" />
-      </a>
+        <a rel="noreferrer noopener" target="_blank" href={URL_INSTAGRAM}>
+          <Image src={instagram} alt="" />
+        </a>
       </div>
       <div className="button wsp">
-      <a rel="noreferrer noopener" target="_blank" 
-              href="https://wa.me/5493704567193?text=¡Hola!%20Quiero%20saber%20más%20sobre%20sus%20travesías%20náuticas." 
-       >
-      <Image src={whatsapp} alt="whatsapp image" />
-      </a>
+        <a
+          rel="noreferrer noopener"
+          target="_blank"
+          href="https://wa.me/5493704567193?text=¡Hola!%20Quiero%20saber%20más%20sobre%20sus%20travesías%20náuticas."
+        >
+          <Image src={whatsapp} alt="whatsapp image" />
+        </a>
       </div>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -1,48 +1,62 @@
- import Head from 'next/head'
-import styles from './Inicio.module.css'; // Importamos los estilos
-import vistosa from '../public/assets/vistosa.jpg';
-import Image from 'next/image';
-import TresBotones from './components/EventosBotones/EventosBotones.jsx';
-import Carrusel from './components/Carrusel/Carrusel.jsx';
-import WeatherWidget from './components/WeatherWidget/WeatherWidget';
+import Head from "next/head";
+import styles from "./Inicio.module.css"; // Importamos los estilos
+import vistosa from "../public/assets/vistosa.jpg";
+import Image from "next/image";
+import TresBotones from "./components/EventosBotones/EventosBotones.jsx";
+import Carrusel from "./components/Carrusel/Carrusel.jsx";
+import WeatherWidget from "./components/WeatherWidget/WeatherWidget";
 
 const Inicio = () => {
   return (
-      <>
-         <Head>
+    <>
+      <Head>
         <title>Nautica Formosa - Canotaje y Travesías en Formosa</title>
-        <meta name="description" content="Disfruta experiencias náuticas únicas en Formosa, Argentina. Canotaje, travesías y más con Nautica Ajere Ysyripe." />
-        <meta name="keywords" content="nautica, Formosa, canotaje, travesías, experiencias náuticas, Argentina" />
+        <meta
+          name="description"
+          content="Disfruta experiencias náuticas únicas en Formosa, Argentina. Canotaje, travesías y más con Nautica Ajere Ysyripe."
+        />
+        <meta
+          name="keywords"
+          content="nautica, Formosa, canotaje, travesías, experiencias náuticas, Argentina"
+        />
         <meta name="author" content="Nautica Ajere Ysyripe" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Nautica Formosa - Canotaje y Travesías" />
-        <meta property="og:description" content="Vive aventuras únicas en Formosa, Argentina. Canotaje y travesías inolvidables." />
+        <meta
+          property="og:title"
+          content="Nautica Formosa - Canotaje y Travesías"
+        />
+        <meta
+          property="og:description"
+          content="Vive aventuras únicas en Formosa, Argentina. Canotaje y travesías inolvidables."
+        />
         <meta property="og:image" content="/assets/vistosa.jpg" />
         <meta property="og:url" content="https://www.tudominio.com" />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <div className={styles.container}>
-      {/* Contenedor de la imagen */}
-      <div className={styles.imageContainer}>
-        <Image
-          src={vistosa}
-          alt="fondo vistoso"
-          className={styles.image}
-          layout="fill" // Llenar el contenedor
-          priority={true} // Optimiza la carga
-        />
+        {/* Contenedor de la imagen */}
+        <div className={styles.imageContainer}>
+          <Image
+            src={vistosa}
+            alt="fondo vistoso"
+            className={styles.image}
+            layout="fill" // Llenar el contenedor
+            priority={true} // Optimiza la carga
+          />
+        </div>
+
+        {/* Contenido adicional */}
+        <h1 className={styles.title}>
+          Nautica Ajere Ysyrype - Canotaje en Formosa, Argentina
+        </h1>
+        <h2 className={styles.description}>
+          Experiencias náuticas, ¿te sumás?
+        </h2>
+        <TresBotones />
+        <Carrusel />
+        {/* <WeatherWidget /> */}
       </div>
-      
-      {/* Contenido adicional */}
-      <h1 className={styles.title}>
-        Nautica Ajere Ysyripe - Canotaje en Formosa, Argentina
-      </h1>
-      <h2 className={styles.description}>Experiencias náuticas, ¿te sumás?</h2>
-      <TresBotones />
-      <Carrusel />
-      {/* <WeatherWidget /> */}
-    </div>
-      </>
+    </>
   );
 };
 
